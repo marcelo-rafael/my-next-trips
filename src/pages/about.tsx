8e828859fd3e3bfd34f46d96ef6f9dd2979f6f36
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-02 01:06:29 -0300

    Create generic PageTemplate to N Pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,37 @@
-import client from 'graphql/client'
 import { GET_PAGES } from 'graphql/queries'
-import AboutTemplate from 'templates/About'
+import { useRouter } from 'next/dist/client/router'
+
+import client from 'graphql/client'
+import PageTemplate from 'templates/Pages'
 
 export default function AboutPage() {
-  return <AboutTemplate />
+  const router = useRouter()
+
+  // retorna um loading, qualquer coisa enquanto está sendo criado
+  if (router.isFallback) return null
+
+  return <PageTemplate />
 }
 
-export const getStaticProps = async () => {
-  const { pages } = await client.request(GET_PAGES)
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
 
-  console.log(pages)
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
 
-  return {
-    props: {}
-  }
+  return { paths, fallback: true }
 }
+
+// export const getStaticProps = async () => {
+//   const { pages } = await client.request(GET_PAGES)
+
+//   console.log(pages)
+
+//   return {
+//     props: {}
+//   }
+// }
 
 // getStaticPaths => serve para gerar as urls em build time /about, /trip/são paulo
 // getStaticProps => server para buscar dados da página (props) - build time - estático
