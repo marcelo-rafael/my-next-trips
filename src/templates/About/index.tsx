1d2014f6a7fbe534b5210fea14665539637bb437
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-01 22:19:54 -0300

    Add About Page

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,25 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+
+    <S.Heading>My next trips</S.Heading>
+
+    <S.Body>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores veniam
+        est perspiciatis laudantium quia eum. Quos totam reiciendis nostrum
+        dignissimos pariatur quibusdam doloribus odio nemo, velit, est voluptas
+        fugit eius.
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
